9073232d2dc7fbcd659e06b07a7ab2f6ce8e3de7
Finn Wohner <finnjwohner@gmail.com>
2025-01-05 21:14:53 +0000

    CSS tweaks

--- src/public/menu_cl.js
+++ src/public/menu_cl.js
@@ -1,57 +1,61 @@
 const socket = io();
 
-const startBtn = document.querySelector('#start-btn');
-const joinBtn = document.querySelector('#join-btn');
-const validCodeText = document.querySelector('#valid-code-text');
-const codeJoinBtn = document.querySelector('#code-join-btn');
-const codeInput = document.querySelector('section .form input');
-const codeJoinForm = document.querySelector('section.code-join-form');
-const codeJoinFormBackBtn = document.querySelector('section.code-join-form button');
+const startBtn = document.querySelector("#start-btn");
+const joinBtn = document.querySelector("#join-btn");
+const validCodeText = document.querySelector("#valid-code-text");
+const codeJoinBtn = document.querySelector("#code-join-btn");
+const codeInput = document.querySelector("section .form input");
+const codeJoinForm = document.querySelector("section.code-join-form");
+const codeJoinFormBackBtn = document.querySelector(
+  "section.code-join-form #back-btn"
+);
 
-startBtn.addEventListener('mousedown', () => {
-    console.log('Sending request to web server to start a new game.')
-    socket.emit('requestStartGame');
-})
-
-socket.on('acceptStartGameRequest', newRoomCode => {
-    window.location.replace(`./${newRoomCode}`);
-})
-
-codeJoinForm.style.display = 'none';
-joinBtn.addEventListener('mousedown', () => {
-    codeJoinForm.style.display = 'flex';
-    codeInput.focus();
-    codeInput.click();
+startBtn.addEventListener("mousedown", () => {
+  console.log("Sending request to web server to start a new game.");
+  socket.emit("requestStartGame");
 });
 
-codeJoinFormBackBtn.addEventListener('mousedown', () => {
-    codeJoinForm.style.display = 'none';
+socket.on("acceptStartGameRequest", (newRoomCode) => {
+  window.location.replace(`./${newRoomCode}`);
 });
 
-codeJoinForm.addEventListener('mousedown', e => {
-    if (e.target !== codeJoinForm) { return; }
-    codeJoinForm.style.display = 'none';
-})
+codeJoinForm.style.display = "none";
+joinBtn.addEventListener("mousedown", () => {
+  codeJoinForm.style.display = "flex";
+  codeInput.focus();
+  codeInput.click();
+});
+
+codeJoinFormBackBtn.addEventListener("mousedown", () => {
+  codeJoinForm.style.display = "none";
+});
+
+codeJoinForm.addEventListener("mousedown", (e) => {
+  if (e.target !== codeJoinForm) {
+    return;
+  }
+  codeJoinForm.style.display = "none";
+});
 
 const enterCode = () => {
-    code = codeInput.value.trim();
-    if (code.length == 5) {
-        window.location.replace(`./${codeInput.value}`);
-    } else {
-        validCodeText.style.visibility = 'visible';
-    }
-}
+  code = codeInput.value.trim();
+  if (code.length == 5) {
+    window.location.replace(`./${codeInput.value}`);
+  } else {
+    validCodeText.style.visibility = "visible";
+  }
+};
 
-codeJoinBtn.addEventListener('mousedown', enterCode);
+codeJoinBtn.addEventListener("mousedown", enterCode);
 
-document.addEventListener('keyup', e => {
-    if (e.key === 'Escape') {
-        codeJoinForm.style.display = 'none';
-    }
-})
+document.addEventListener("keyup", (e) => {
+  if (e.key === "Escape") {
+    codeJoinForm.style.display = "none";
+  }
+});
 
-document.addEventListener('keydown', e => {
-    if (e.key === 'Enter' && codeJoinForm.style.display == 'flex')  {
-        enterCode();
-    }
-})
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Enter" && codeJoinForm.style.display == "flex") {
+    enterCode();
+  }
+});
